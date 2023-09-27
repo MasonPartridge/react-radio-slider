96b4ffe48665c0e307fd589d04a292e8110eccc2
MasonPartridge <masonepartridge@gmail.com>
2023-09-26 18:11:13 -0700

    Create ReactRadioSlider component

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-
+import ReactRadioSlider from './components/ReactRadioSlider';
+import { useState } from "react";
 function App() {
+const [value, setValue] = useState(50);
+const radioOptions = [
+  <div className='red-box'></div>,
+  <div className='red-box'></div>,
+  <div className='red-box'></div>,
+];
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App App-header">
+      <ReactRadioSlider onChange={setValue} value={value} radioOptions={radioOptions} optionWidth={300}/>
     </div>
   );
 }
---
+++ src/components/ReactRadioSlider.tsx
@@ -0,0 +1,93 @@
+import React, { ReactNode, useState, useEffect } from "react";
+
+export default function ReactRadioSlider(props: {
+  value: number;
+  onChange: (value: number) => void;
+  radioOptions: ReactNode[];
+  optionWidth: number;
+  deselectedOpacity?: number;
+  optionHeight?: number;
+  max?: number;
+  min?: number;
+}) {
+  const {
+    value,
+    onChange,
+    radioOptions,
+    optionWidth,
+    max,
+    min,
+    optionHeight,
+    deselectedOpacity,
+  } = props;
+  const [sliderPixelWidth, setSliderPixelWidth] = useState(0);
+  const [localValue, setLocalValue] = useState(value);
+
+  useEffect(() => {
+    setSliderPixelWidth(optionWidth * radioOptions.length);
+  }, [optionWidth, radioOptions.length]);
+
+  useEffect(() => {
+    setLocalValue(
+      (value * sliderPixelWidth) / ((max ?? 100) - (min ?? 0)) + (min ?? 0)
+    );
+  }, [value, max, min, sliderPixelWidth]);
+
+  function convertLocalValueToValue(input: number): number {
+    const _min = min ?? 0;
+    return ((input - _min) * ((max ?? 100) - _min)) / sliderPixelWidth;
+  }
+
+  function handleSliderChange(
+    event: React.ChangeEvent<HTMLInputElement>
+  ): void {
+    onChange(Math.round(convertLocalValueToValue(event.target.valueAsNumber)));
+  }
+
+  function handlesRadioOptionClick(index: number): void {
+    onChange(
+      Math.round(
+        convertLocalValueToValue(index * optionWidth + optionWidth / 2)
+      )
+    );
+  }
+  return (
+    <div style={{ display: "flex", flexDirection: "column" }}>
+      <div style={{ display: "flex" }}>
+        {radioOptions.map((radioOption, index) => {
+          return (
+            <div
+              style={{
+                width: optionWidth,
+                height: optionHeight ?? "auto",
+                overflow: "hidden",
+                opacity:
+                  localValue > optionWidth * (index + 1) ||
+                  localValue < optionWidth * index
+                    ? `${deselectedOpacity ?? "50"}%`
+                    : "100%",
+              }}
+              key={index}
+              onClick={() => {
+                handlesRadioOptionClick(index);
+              }}
+            >
+              <p>{index * optionWidth}</p>
+              {radioOption}
+            </div>
+          );
+        })}
+      </div>
+      <input
+        type="range"
+        min="0"
+        max={sliderPixelWidth}
+        onChange={handleSliderChange}
+        value={localValue}
+        style={{ width: "100%" }}
+      ></input>
+      <h1>{value}</h1>
+      <h1>{localValue}</h1>
+    </div>
+  );
+}
